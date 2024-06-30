c18b3828c708f6e7593d9b656f6216ae017878f1
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-06-30 17:25:47 +0400

    fix routing

--- geo-app-admin/src/entities/tasks/api/index.ts
+++ geo-app-admin/src/entities/tasks/api/index.ts
@@ -2,8 +2,8 @@ import { http } from "@shared"
 import { BaseTask } from "../model/index.d"
 
 
-export const postTask = async (topicId: number, item: Omit<BaseTask, 'id'>) => (await http<BaseTask>({ url: '/adm/sections/topics/' + topicId + '/tasks', method: 'POST', data: item })).data
+export const postTask = async (topicId: number, item: Omit<BaseTask, 'id'>) => (await http<BaseTask>({ url: '/adm/topics/' + topicId + '/tasks', method: 'POST', data: item })).data
 
-export const patchTask = async (topicId: number, id: number, item: Omit<BaseTask, 'id'>) => (await http<BaseTask>({ url: '/adm/sections/topics/' + topicId + '/tasks/' + id, method: 'PATCH', data: item })).data
+export const patchTask = async (topicId: number, id: number, item: Omit<BaseTask, 'id'>) => (await http<BaseTask>({ url: '/adm/topics/' + topicId + '/tasks/' + id, method: 'PATCH', data: item })).data
 
-export const deleteTask = async (topicId: number, id: number) => (await http<BaseTask>({ url: '/adm/sections/topics/' + topicId + '/tasks/' + id, method: 'DELETE', })).data
+export const deleteTask = async (topicId: number, id: number) => (await http<BaseTask>({ url: '/adm/topics/' + topicId + '/tasks/' + id, method: 'DELETE', })).data
--- geo-app-admin/src/entities/topics/api/index.ts
+++ geo-app-admin/src/entities/topics/api/index.ts
@@ -2,12 +2,12 @@ import { http } from "@shared";
 import { BaseTopic, BaseTopicWithTasks } from "../model/index.d";
 
 export const getTopics = async () => {
-    const data = (await http<BaseTopic[]>({ url: '/adm/sections/topics' })).data;
+    const data = (await http<BaseTopic[]>({ url: '/adm/topics' })).data;
     return data
 }
-export const getTopic = async (id: number) => (await http<BaseTopicWithTasks>({ url: '/adm/sections/topics/' + id })).data;
-export const postTopic = async (item: Omit<BaseTopic, 'id'>) => (await http<BaseTopic>({ url: '/adm/sections/topics', method: 'POST', data: item })).data;
+export const getTopic = async (id: number) => (await http<BaseTopicWithTasks>({ url: '/adm/topics/' + id })).data;
+export const postTopic = async (item: Omit<BaseTopic, 'id'>) => (await http<BaseTopic>({ url: '/adm/topics', method: 'POST', data: item })).data;
 
-export const patchTopic = async (id: number, item: Omit<BaseTopic, 'id'>) => (await http<BaseTopic>({ url: '/adm/sections/topics/' + id, method: 'PATCH', data: item, })).data;
+export const patchTopic = async (id: number, item: Omit<BaseTopic, 'id'>) => (await http<BaseTopic>({ url: '/adm/topics/' + id, method: 'PATCH', data: item, })).data;
 
-export const deleteTopic = async (id: number) => (await http<{ ok: boolean }>({ method: 'DELETE', url: '/adm/sections/topics/' + id })).data;
+export const deleteTopic = async (id: number) => (await http<{ ok: boolean }>({ method: 'DELETE', url: '/adm/topics/' + id })).data;
