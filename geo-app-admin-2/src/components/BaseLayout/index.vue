12c9e923e8d39f7adb01279ae16cbca64d87ae33
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-07-01 23:38:10 +0400

    add logout button

--- geo-app-admin-2/src/components/BaseLayout/index.vue
+++ geo-app-admin-2/src/components/BaseLayout/index.vue
@@ -8,6 +8,9 @@
             <v-app-bar-nav-icon @click="isShowDrawer = !isShowDrawer"></v-app-bar-nav-icon>
 
             <v-app-bar-title>GEO App Admin</v-app-bar-title>
+            <v-spacer></v-spacer>
+            <span>{{  authStore.user.username }}</span>
+            <v-btn @click="authStore.logout">Выйти</v-btn>
         </v-app-bar>
 
         <v-main>
@@ -19,5 +22,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import SidebarMenu from '@/components/SidebarMenu/index.vue'
+import { useAuthStore } from '@/stores/auth';
 const isShowDrawer = ref(false)
+const authStore = useAuthStore()
 </script>
