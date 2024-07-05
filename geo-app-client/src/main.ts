d0bd1e57e6242b2d02ceee15ae789edc50950736
Mihail Pogosyan <mihail.pogosyan96@gmail.com>
2024-07-05 18:12:39 +0400

    localize dayjs

--- geo-app-client/src/main.ts
+++ geo-app-client/src/main.ts
@@ -1,10 +1,13 @@
+import 'dayjs/locale/ru'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import { vuetify } from './plugins/vuetify'
+import dayjs from 'dayjs'
 
+dayjs.locale('ru')
 const app = createApp(App)
 
 app.use(vuetify)
